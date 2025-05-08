1c9a993a185f2d3b6fa04534be3451c5b73ad84a
Aj <53246298+alibecerra@users.noreply.github.com>
2025-05-08 00:52:30 -0700

    added win condition

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -7,22 +7,42 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public GameObject winScreen;
+    public int winScore = 20;
+    public bool gameWon = false;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
+        if (gameWon) return;
+
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
+
+        if (playerScore >= winScore)
+        {
+            winGame();
+        }
     }
 
     public void restartGame()
     {
+        Time.timeScale = 1; // Resume game in case it was paused
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void gameOver()
     {
-        gameOverScreen.SetActive(true);
+        if (!gameWon)
+        {
+            gameOverScreen.SetActive(true);
+        }
     }
 
+    public void winGame()
+    {
+        gameWon = true;
+        Time.timeScale = 0; // Pause game
+        winScreen.SetActive(true);
+    }
 }
